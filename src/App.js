21c86d85b2ba43360622a2459731a4efe34ac5f2
huynvn010193 <hpn010193@gmail.com>
2021-07-18 10:14:50 +0700

    24: LapTodo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
+import TodoFeature from "./features/Todo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>HuyNVN</p>
-      </header>
+      <TodoFeature />
     </div>
   );
 }
