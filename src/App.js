db0a532704551dd7afc2e4d61c49578bc8a7eaad
huynvn010193 <hpn010193@gmail.com>
2021-07-19 14:37:39 +0700

    43. Tao router tung feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import AlbumFeature from "./features/Album";
+import { Route } from "react-router-dom";
 import TodoFeature from "./features/Todo";
+import AlbumFeature from "./features/Album";
 
 function App() {
   return (
     <div className="App">
-      <TodoFeature />
+      Home Page
       {/* <AlbumFeature/> */}
+      <Route path="/todos" component={TodoFeature} />
+      <Route path="/album" component={AlbumFeature} />
     </div>
   );
 }
