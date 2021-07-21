465ea00631404ee362469968649526f620aef6a9
huynvn010193 <hpn010193@gmail.com>
2021-07-21 21:51:19 +0700

    64.Tao custom field: InputField

--- src/components/form-controls/InputField/index.jsx
+++ src/components/form-controls/InputField/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
+import { Controller } from 'react-hook-form';
 
 InputField.propTypes = {
   form: PropTypes.object.isRequired,
@@ -12,9 +13,14 @@ InputField.propTypes = {
 function InputField(props) {
   const { form, name, label, disabled } = props;
   return (
-    <div>
-      <TextField fullWidth />
-    </div>
+    <Controller
+      name={name}
+      control={form.control}
+      as={TextField}
+      fullWidth
+      label={label}
+      disabled={disabled}
+    />
   );
 }
 
