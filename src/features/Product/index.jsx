4b2458b6397b5123bfee042ee5bae9f29fa5974e
huynvn010193 <hpn010193@gmail.com>
2021-07-26 23:28:00 +0700

    115.Setup Layout ProductList

--- src/features/Product/index.jsx
+++ src/features/Product/index.jsx
@@ -1,20 +1,18 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import ListProductPage from './pages/ListProductPage';
 
-ProductFeature.propTypes = {
-
-};
+ProductFeature.propTypes = {};
 
 function ProductFeature(props) {
   const match = useRouteMatch();
   return (
-    <div>
-      Product Feature
+    <Box pt={4}>
       <Switch>
         <Route path={match.url} exact component={ListProductPage} />
       </Switch>
-    </div>
+    </Box>
   );
 }
 
--- src/features/Product/pages/ListProductPage.jsx
+++ src/features/Product/pages/ListProductPage.jsx
@@ -1,17 +1,31 @@
-import { Box, Container, Grid } from '@material-ui/core';
+import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import React from 'react';
 
-ListProductPage.propTypes = {
+ListProductPage.propTypes = {};
 
-};
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  left: {
+    width: '250px',
+  },
+  right: {
+    flex: '1 1 auto',
+    marginLeft: '5px',
+  },
+}));
 
 function ListProductPage(props) {
+  const classes = useStyles();
   return (
     <Box>
       <Container>
         <Grid container>
-          <Grid item>Left Column</Grid>
-          <Grid item>Right Column</Grid>
+          <Grid item className={classes.left} spacing={1}>
+            <Paper elevation={0}>Left Column</Paper>
+          </Grid>
+          <Grid item className={classes.right}>
+            <Paper elevation={0}>Right Column</Paper>
+          </Grid>
         </Grid>
       </Container>
     </Box>
