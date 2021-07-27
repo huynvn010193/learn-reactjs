5784e11d8866216a38249f2e9d0b116182e70db6
huynvn010193 <hpn010193@gmail.com>
2021-07-27 16:45:23 +0700

    117.Lam skeleton cho ds san pham

--- src/features/Product/pages/ListProductPage.jsx
+++ src/features/Product/pages/ListProductPage.jsx
@@ -1,6 +1,7 @@
-import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
+import { Box, Container, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
 import productApi from 'api/productApi';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductSkeletonList from '../components/ProductSkeletonList';
 
 ListProductPage.propTypes = {};
 
@@ -17,23 +18,30 @@ const useStyles = makeStyles((theme) => ({
 
 function ListProductPage(props) {
   const classes = useStyles();
+  const [productList, setProductList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
-      const response = await productApi.getAll({ _page: 1, _limit: 10 });
-      console.log(response);
-    })()
+      try {
+        const { data } = await productApi.getAll({ _page: 1, _limit: 10 });
+        setProductList(data);
+      } catch (error) {
+        console.log('Faile to fecth product List', error);
+      }
+      setLoading(false);
+    })();
   }, []);
 
   return (
     <Box>
       <Container>
         <Grid container>
-          <Grid item className={classes.left} spacing={1}>
+          <Grid item className={classes.left}>
             <Paper elevation={0}>Left Column</Paper>
           </Grid>
           <Grid item className={classes.right}>
-            <Paper elevation={0}>Right Column</Paper>
+            <Paper elevation={0}>{loading ? <ProductSkeletonList /> : <Typography>Product List</Typography>}</Paper>
           </Grid>
         </Grid>
       </Container>
