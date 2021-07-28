aa7966b05ccdbf91552837de2b01e8fe4566fb4e
huynvn010193 <hpn010193@gmail.com>
2021-07-28 23:49:12 +0700

    127> Loc theo khoang gia

--- src/features/Product/components/ProductFilter.jsx
+++ src/features/Product/components/ProductFilter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '@material-ui/core';
 import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
 
 ProductFilter.propTypes = {
   filters: PropTypes.object.isRequired,
@@ -12,16 +13,21 @@ function ProductFilter({ filters, onChange }) {
   const handleCategoryChange = (newCategoryId) => {
     if (!onChange) return;
     const newFilters = {
-      ...filters,
       'category.id': newCategoryId,
     };
     onChange(newFilters);
   };
 
+  const handlePriceChange = (values) => {
+    if (onChange) {
+      onChange(values);
+    }
+  };
+
   return (
     <Box>
       <FilterByCategory onChange={handleCategoryChange} />
-      {/* <FilterByPrice onChange={handleCategoryChange} /> */}
+      <FilterByPrice onChange={handlePriceChange} />
     </Box>
   );
 }
