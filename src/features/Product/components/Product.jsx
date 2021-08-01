956193ddc76a9f2c2e02b5f389d7d900f2af42cf
huynvn010193 <hpn010193@gmail.com>
2021-08-01 15:15:20 +0700

    139.Code component ProductInfo

--- src/features/Product/components/Product.jsx
+++ src/features/Product/components/Product.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Box, Typography } from '@material-ui/core';
 import { STATIC_HOST, THUMBNAIL_PLACEHOLDER } from 'constants/index';
+import { useHistory } from 'react-router-dom';
+import { formatPrice } from 'utils';
 
 Product.propTypes = {
   product: PropTypes.object,
@@ -9,15 +11,20 @@ Product.propTypes = {
 
 function Product({ product }) {
   const thumbnaiUrl = product.thumbnail ? `${STATIC_HOST}${product.thumbnail?.url}` : THUMBNAIL_PLACEHOLDER;
+  const history = useHistory();
+  const handleClickProduct = () => {
+    history.push(`/products/${product.id}`);
+  };
+
   return (
-    <Box padding={1}>
+    <Box padding={1} onClick={handleClickProduct}>
       <Box padding={1} minHeight="215px">
         <img src={thumbnaiUrl} alt={product.name} width="100%" />
       </Box>
       <Typography variant="body2">{product.name}</Typography>
       <Typography variant="body2">
         <Box component="span" fontSize="16px" fontWeight="bold" marginRight={1}>
-          {new Intl.NumberFormat('vn-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
+          {formatPrice(product.salePrice)}
         </Box>
         {product.promotionPercent > 0 && `- ${product.promotionPercent}%`}
       </Typography>
--- src/features/Product/pages/DetailPage.jsx
+++ src/features/Product/pages/DetailPage.jsx
@@ -1,36 +1,49 @@
 import React from 'react';
 import { Box, Container, Grid, makeStyles, Paper } from '@material-ui/core';
 import ProductThumbnail from '../components/ProductThumbnail';
+import { useRouteMatch } from 'react-router-dom';
+import useProductDetail from '../hooks/useProductDetail';
+import ProductInfo from '../components/ProductInfo';
 
-DetailProductPage.propTypes = {
-
-};
+DetailProductPage.propTypes = {};
 
 const useStyles = makeStyles((theme) => ({
   root: {},
   left: {
     width: '400px',
     padding: theme.spacing(1.5),
-    borderRight: `1px solid ${theme.palette.grey[300]}`
+    borderRight: `1px solid ${theme.palette.grey[300]}`,
   },
   right: {
     flex: '1 1 0',
-    padding: theme.spacing(1.5)
+    padding: theme.spacing(1.5),
   },
 }));
 
 function DetailProductPage() {
   const classes = useStyles();
+  const {
+    params: { productId },
+  } = useRouteMatch();
+
+  const { product, loading } = useProductDetail(productId);
+
+  if (loading) {
+    <Box>Loading</Box>;
+  }
+
+  console.log('product', product);
+
   return (
     <Box className={classes.root}>
       <Container>
         <Paper elevation={0}>
           <Grid container>
             <Grid item className={classes.left}>
-              <ProductThumbnail product={{}} />
+              <ProductThumbnail product={product} />
             </Grid>
             <Grid item className={classes.right}>
-              Product Info
+              <ProductInfo product={product} />
             </Grid>
           </Grid>
         </Paper>
